8b5cf3bca80069e179d2e4246fa93b469a718d8b
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-05 21:02:10 -0700

    created index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "/", to: "homepages#index"
   root to: "homepages#index"
   resources :works
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :create, :show]
 end
