eeb952308ef464a90377291d861b8369b3d32f85
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-05 20:45:26 -0700

    added view files

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "/", to: "homepages#index"
   root to: "homepages#index"
   resources :works
-  resources :users, only: [:index, :new]
+  resources :users, only: [:index, :new, :create]
 end
